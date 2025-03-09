398865d7eec5a55edf372c8585e1c91c5f2fdeb0
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-03-09 12:19:57 +0600

    lil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
-
-import aiogram
+import random
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -12,18 +11,59 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+# secret_number = random.randint(1, 100)
+# count = 0
+
+
 @dp.message(Command('start'))
 async def send_message(message: Message):
     await message.answer(f'Hello, {message.from_user.username}😊, Соз жаз')
 
 
+# @dp.message()
+# async def check_number(message: Message):
+#     word = message.text.lower()
+#     if word == word[::-1]:
+#         await message.answer('Palindrom')
+#     else:
+#         await message.answer('Palindrom emes')
+
+
+
+
+
+
+# @dp.message()
+# async def check_number(message: Message):
+#     global count
+#     number = int(message.text)
+#     count += 1
+#
+#     if number > secret_number:
+#         await message.answer('Сиз жазган сан чон')
+#     elif number < secret_number:
+#         await message.answer('Сиз жазган сан кичине')
+#     else:
+#         await message.answer(f'Куттуктайбыз сиз {secret_number} санын {count} иретте таптыныз')
+#
+
+
+
+
 @dp.message()
-async def check_palindrom(message: Message):
-    word = message.text.lower()
-    if word == word[::-1]:
-        await message.answer('Palindrom')
-    else:
-        await message.answer('Palindrom emes')
+async def check_number(message: Message):
+    number_one = int(message.text)
+    number_two = int(message.text)
+    znak = await message.answer('+ - * / : ')
+
+    if znak == '+':
+        await message.answer(number_one + number_two)
+    elif znak == '-':
+        await message.answer(number_one - number_two)
+    elif znak == '*':
+        await message.answer(number_one * number_two)
+    elif znak == '/':
+        await message.answer(number_one / number_two)
 
 
 async def main():
